dcaadce7e77f66361a62267b7766066c7d9e1b23
Robert Csakany <csakany.robert@gmail.com>
2011-05-19 13:37:08 +0200

    Oracle driver added

--- src/main/java/org/liveSense/misc/queryBuilder/jdbcDriver/JdbcDrivers.java
+++ src/main/java/org/liveSense/misc/queryBuilder/jdbcDriver/JdbcDrivers.java
@@ -4,8 +4,12 @@ package org.liveSense.misc.queryBuilder.jdbcDriver;
 public enum JdbcDrivers {
 	MYSQL ("com.mysql.jdbc.Driver"),
 	FIREBIRD ("org.firebirdsql.jdbc.FBDriver"),
-	HSQLDB ("org.hsqldb.jdbcDriver");
+	HSQLDB ("org.hsqldb.jdbcDriver"),
+	ORACLE ("oracle.jdbc.driver.OracleDriver");
 	
+	//TODO: Adding support for following drivers:
+	// postgresql.Driver
+	// com.microsoft.sqlserver.jdbc.SQLServerDriver
 	private final String driverClass;
 	
 	private JdbcDrivers(String driverClass) {

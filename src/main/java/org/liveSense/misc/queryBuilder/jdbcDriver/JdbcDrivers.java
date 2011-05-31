37d8e37ed44a5dfe5693a5b9160a726f925c3161
Robert Csakany <csakany.robert@gmail.com>
2011-06-01 01:14:56 +0200

    Oracle JDBC alternative name added

--- src/main/java/org/liveSense/misc/queryBuilder/jdbcDriver/JdbcDrivers.java
+++ src/main/java/org/liveSense/misc/queryBuilder/jdbcDriver/JdbcDrivers.java
@@ -5,7 +5,8 @@ public enum JdbcDrivers {
 	MYSQL ("com.mysql.jdbc.Driver"),
 	FIREBIRD ("org.firebirdsql.jdbc.FBDriver"),
 	HSQLDB ("org.hsqldb.jdbcDriver"),
-	ORACLE ("oracle.jdbc.driver.OracleDriver");
+	ORACLE ("oracle.jdbc.OracleDriver"),
+	ORACLE2 ("oracle.jdbc.driver.OracleDriver");
 	
 	//TODO: Adding support for following drivers:
 	// postgresql.Driver
